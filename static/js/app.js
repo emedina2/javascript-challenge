5bc68d1b6d011f1f728320ba7eb9d567706d5d93
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-04-06 20:34:58 -0500

    set up event listeners

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,19 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Track events for filter button & date field
+var button = d3.select("#filter-btn");
+var dateField = d3.select("#datetime");
+var enteredDate;
+dateField.on("change", ()=>{
+    enteredDate = d3.event.target.value
+    console.log(enteredDate);
+    console.log(`The changed date is ${enteredDate}.`)
+})
+
+button.on("click", ()=>{
+console.log("Filter button clicked")
+console.log(`The entered date is ${enteredDate}.`)
+});
+
