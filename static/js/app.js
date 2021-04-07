b31cfb7c34e7212b819f029c6c45c62d0e705438
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-04-06 21:10:57 -0500

    Update app.js
    Set up loop to add table data.

--- static/js/app.js
+++ static/js/app.js
@@ -7,17 +7,24 @@ var tableData = data;
 var button = d3.select("#filter-btn");
 var dateField = d3.select("#datetime");
 var enteredDate;
+var sightingTable = d3.select("tbody")
 dateField.on("change", ()=>{
     enteredDate = d3.event.target.value
     console.log(enteredDate);
     console.log(`The changed date is ${enteredDate}.`)
 })
 
+//button actions
 button.on("click", ()=>{
 console.log("Filter button clicked")
 console.log(`The entered date is ${enteredDate}.`)
 var alienSightings = data.filter(findAliens)
 console.log(alienSightings)
+sightingTable.html("")
+for (var s = 0; s < alienSightings.length; s++) {
+    sightingData(alienSightings[s])
+}
+
 });
 
 //create function to filter data
@@ -25,3 +32,18 @@ function findAliens(date) {
     return date.datetime === enteredDate;
 };
 
+//function to loop through data & add it to page
+function sightingData(sightings){
+    
+    sightingTable.append("tr")
+    sightingTable.append("td").text(sightings.datetime)
+    sightingTable.append("td").text(sightings.city)
+    sightingTable.append("td").text(sightings.state)
+    sightingTable.append("td").text(sightings.country)
+    sightingTable.append("td").text(sightings.shape)
+    sightingTable.append("td").text(sightings.durationMinutes)
+    sightingTable.append("td").text(sightings.comments)
+
+
+
+}
